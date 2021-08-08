6ab01e4cf033a7e9e4e3e73c0d5db9468a08a23e
Jana Georgieva <Jana Georgieva@DESKTOP-9PDRBOF>
2021-08-08 22:55:48 +0200

    benchmarks draft

---
+++ tools/BankAccountParser.Benchmarks/Benchmarks/BankAccountParserBenchmark.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BaParser = BankAccountParser.Parser.Parser;
+
+namespace BankAccountParser.Benchmarks.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class BankAccountParserBenchmark
+    {
+        [Benchmark]
+        public void BenchmarkSimpleFormat()
+        {
+            BaParser.parse("p-a/b");
+        }
+    }
+}
---
+++ tools/BankAccountParser.Benchmarks/Program.cs
@@ -0,0 +1,21 @@
+using BankAccountParser.Benchmarks.Benchmarks;
+using BenchmarkDotNet.Running;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAccountParser.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<BankAccountParserBenchmark>();
+        }
+    }
+}
